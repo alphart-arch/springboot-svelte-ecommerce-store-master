05a8f4a394bbdcbae4674ffe1266309bebc88a9c
alphart-start <alphart07@gmail.com>
2022-06-04 15:16:42 +0530

    commit

---
+++ gf-ecommerce-backend/src/main/java/com/gofluent/shoppingcart/model/Item.java
@@ -0,0 +1,45 @@
+package com.gofluent.shoppingcart.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.Id;
+import java.io.Serializable;
+import javax.persistence.GeneratedValue;
+//import org.springframework.stereotype.Component;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class Item implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+    @GeneratedValue
+	private Integer itemID;
+	private String itemName;
+	private Float itemPrice;
+	private Integer itemStockCount;
+	private Float totalPricePerItem=0.02F;
+	private Float totalPricePerBasket=0.02F;
+	private int quantity=0;
+    public void setQuantity(Integer value) {
+    }
+	public Integer getItemPrice() {
+		return null;
+	}
+	public void setTotalPricePerItem(Float totalPricePerItem2) {
+	}
+    public Float getTotalPricePerItem() {
+        return null;
+    }
+    public void setTotalPricePerBasket(Float totalPriceWholeBasket) {
+    }
+
+}
